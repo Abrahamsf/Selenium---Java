6a2da28879c6011c84722bd6c9d2d8f98b2fce1a
Abrahamsf <fuada@hotail.co.za>
2016-01-22 02:18:53 +0200

    Added Selenium Tests

---
+++ Selenium - JavaRC/EclipseWorkspace/TestWiki/src/lib/GlobalVariables.java
@@ -0,0 +1,26 @@
+package lib;
+
+public class GlobalVariables {
+	
+	//Excel Data Path
+	public String getDataPath(){
+		
+		String path = "C:\\Users\\USER\\EclipseWorkspace\\TestData\\"; //change path to machine currently running script
+		return path;
+	}
+	
+	//Wiki Url
+	public String getWikiUrl(){
+		
+		String url = "https://en.wikipedia.org/wiki/Main_Page";
+		return url;
+	}
+	
+	//Json Validator URL
+	public String getValidatorUrl(){
+		
+		String url = "http://www.freeformatter.com/json-validator.html";
+		return url;
+	}
+
+}
